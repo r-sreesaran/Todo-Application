f6c147dc538d07892f711d51836af8aae92e76b2
r-sreesaran <r.sreesaran@gmail.com>
2019-03-17 15:46:53 +0530

    added function for sending pending list

--- emailSender.py
+++ emailSender.py
@@ -1,5 +1,8 @@
 from db_functions import get_incomplete
-htmlformat="<body><tr><th>priority</th><th>task</th></tr>"
+htmlformat= "<html><body><tr><th>priority</th><th>task</th></tr>"
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
     
 
 def getData():
@@ -7,6 +10,32 @@ def getData():
     incompleteItems = get_incomplete()
     for task in incompleteItems:
         htmlformat = htmlformat + "<tr>"+ str(task[0])+"</tr>"+"<tr>"+task[1]+"</tr>"
-    htmlformat = htmlformat +"</body>"    
+    htmlformat = htmlformat +"</body></html>"    
     print(htmlformat)
-getData()
+
+def testEmailSending():
+         getData()
+         global htmformat
+         sender_email = "sreesaran.developer@gmail.com"
+         receiver_email = "r.sreesaran@gmail.com"
+         password = "Xtech4582"
+
+         message = MIMEMultipart("alternative")
+         message["Subject"] = "Pending List"
+         message["From"] = sender_email
+         message["To"] = receiver_email
+         part1 = MIMEText("Please check out the pending list and try to progress on them", "Plain")
+         part2 = MIMEText(htmlformat, "html")
+         message.attach(part1)
+         message.attach(part2)
+
+     
+         context = ssl.create_default_context()
+         with  smtplib.SMTP_SSL("smtp.gmail.com", "465",context=context) as server:
+                server.login("sreesaran.developer@gmail.com", password)
+                server.sendmail("sreesaran.developer@gmail.com", "r.sreesaran@gmail.com", message.as_string())
+                server.quit()
+            
+
+#getData()
+testEmailSending()
